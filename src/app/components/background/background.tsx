87944d841c9eb6a293c6a6d02aa082c2134e4332
Avalons <maikelflorentio@gmail.com>
2025-11-14 21:03:18 +0700

    replace all asset to fetch from github

--- src/app/components/background/background.tsx
+++ src/app/components/background/background.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export default function Background() {
   return (
     <div id="back">
-      <img src="/img18.JPG" alt="Lapalma Padel Club BSD Court" />
-      <img src="/img1.JPG" alt="Lapalma Padel Club BSD Lounge" />
+      <img src="https://raw.githubusercontent.com/FatalFriction/lapalma-padelclub/master/public/img18.JPG" alt="Lapalma Padel Club BSD Court" />
+      <img src="https://raw.githubusercontent.com/FatalFriction/lapalma-padelclub/master/public/img1.JPG" alt="Lapalma Padel Club BSD Lounge" />
     </div>
   );
 }
