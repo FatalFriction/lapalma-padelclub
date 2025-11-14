fffaf87ccf2cc34ff5d8a3ef0f99466a97574fd2
Avalons <maikelflorentio@gmail.com>
2025-11-14 21:11:12 +0700

    replace to static CDN Github

--- src/app/components/background/background.tsx
+++ src/app/components/background/background.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export default function Background() {
   return (
     <div id="back">
-      <img src="https://raw.githubusercontent.com/FatalFriction/lapalma-padelclub/master/public/img18.JPG" alt="Lapalma Padel Club BSD Court" />
-      <img src="https://raw.githubusercontent.com/FatalFriction/lapalma-padelclub/master/public/img1.JPG" alt="Lapalma Padel Club BSD Lounge" />
+      <img src="https://fatalfriction.github.io/lapalma-padelclub/img18.jpg" alt="Lapalma Padel Club BSD Court" />
+      <img src="https://fatalfriction.github.io/lapalma-padelclub/img1.jpg" alt="Lapalma Padel Club BSD Lounge" />
     </div>
   );
 }
